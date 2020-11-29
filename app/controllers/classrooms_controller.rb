3526c1adaf545e4907465b90659c871f3507c140
keesha <kharg14@gmail.com>
2020-11-28 19:45:54 -0500

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,23 @@
 class ClassroomsController < ApplicationController
   def show
-    @classroom = Classroom.find(params[:id])
+    @classroom = set_classroom
     @student = @classroom.oldest_student
+    @students = @classroom.students
   end
 
   def index
     @classrooms = Classroom.all
+  end
+
+  private
+  def set_classroom
+    Classroom.find(params[:id])
+  end
+  def classroom_params
+    params.require(:classroom).permit(
+      :course_name,
+      :semester,
+      :classroom
+    )
   end
 end
